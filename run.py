c6442d62986324dc4ef51291b6bdcbffb54e52e0
Rafael Greca <rgvieira97@gmail.com>
2024-07-22 09:28:12 -0300

    updating readme and run files

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
 import mlflow
 import argparse
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--experiment_name", "-n", type=str, required=True)
     args = parser.parse_args()
 
     mlflow.projects.run(
-        uri="https://github.com/rafaelgreca/mlflow-example",
-        version="main",
+        uri=".",
         parameters=None,
-        experiment_name=args.experiment_name,
+        experiment_name=args.experiment_name
     )
